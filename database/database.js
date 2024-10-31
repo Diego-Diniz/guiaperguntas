bc9d0a5f64eae2aa2986418f4266737368231a5d
Diego Henrique Diniz <115659288+Diego-Diniz@users.noreply.github.com>
2024-10-31 13:20:54 -0300

    Removendo arquivo de credenciais do histórico

--- database/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const connection = new Sequelize('guiaperguntas','root','Homer987@@',{
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = connection;
